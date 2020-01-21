98a69433f668404edbb9d2ebcbed5cbe9cdeec4b
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-01-21 19:40:19 +0530

    added test case to understand split expenses algorithm

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import "./styles.css";
 
-const expensesList = [{
-  name: "Eleven",
-  expenses: [20, 40, 100]
-},
-{
-  name: "Max",
-  expenses:[80]
+const expensesList = [
+  {
+    name: "Eleven",
+    expenses: [20, 40, 100]
+  },
+  {
+    name: "Max",
+    expenses: [80]
+  }
+];
+
+function addNumbers(arr) {
+  return arr.reduce((acc, cur) => acc + cur);
 }
-]
 
-
-
+/*
+Eleven paid for flights: 500
+Max paid for flights: 0
+Total expenses: 500
+expenses per person(EPP): 250
+outstanding (Eleven) :  EPP - 500 = -250
+outstanding (Max): EPP - 0 = +250
+*/
