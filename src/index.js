90d60dd217b8ab41b85522b000674b637936f2fb
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-01-27 20:52:40 +0530

    started working on modal

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
 import "./styles.css";
 
-const expensesList = [
-  {
-    name: "Eleven",
-    expenses: [20, 40, 100]
-  },
-  {
-    name: "Max",
-    expenses: [80]
-  }
-];
+const createGoDutchApp = function() {};
 
-function addNumbers(arr) {
-  return arr.reduce((acc, cur) => acc + cur);
-}
-
-/*
-Eleven paid for flights: 500
-Max paid for flights: 0
-Total expenses: 500
-expenses per person(EPP): 250
-outstanding (Eleven) :  EPP - 500 = -250
-outstanding (Max): EPP - 0 = +250
-*/
+const GoDutchApp = new createGoDutchApp();
