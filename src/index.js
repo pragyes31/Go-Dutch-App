ffff66e8c8b3a2aedad733c9bc4335c84eee3356
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-01-27 21:39:15 +0530

    createGoDutchApp function added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import "./styles.css";
 
-const createGoDutchApp = function() {};
+const createGoDutchApp = function() {
+  const newTripBtn = document.querySelector("#new-trip-button");
+  const newTripModal = document.querySelector("#new-trip-modal");
+  const toggleModal = function() {
+    let isOpen = false;
+  };
+  newTripBtn.addEventListener("click", toggleModal);
+};
 
 const GoDutchApp = new createGoDutchApp();
