04b803a8a44631922933aca56c1ce28e1656d7b4
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-01-28 16:19:29 +0530

    added logic for closing modal by clicking anywhere on the screen

--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ const createGoDutchApp = function() {
   const closeModalBtn = document.querySelector("#close-modal");
   let isModalOpen = false;
   const toggleModal = function() {
+    console.log("boom");
     isModalOpen
       ? (newTripModal.style.display = "none")
       : (newTripModal.style.display = "block");
     newTripBtn.disabled = !isModalOpen;
     isModalOpen = !isModalOpen;
   };
+  const closeModal = function(e) {
+    if (!e.target.matches("#new-trip-modal, #new-trip-button") && isModalOpen)
+      toggleModal();
+  };
   newTripBtn.addEventListener("click", toggleModal);
   closeModalBtn.addEventListener("click", toggleModal);
+  document.addEventListener("click", closeModal);
 };
 
 const GoDutchApp = new createGoDutchApp();
