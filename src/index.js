f6c76c7fb34d6cb09f547abfeba9ad2be6528129
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-01-28 13:32:50 +0530

    added logic for modal window toggle

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import "./styles.css";
 const createGoDutchApp = function() {
   const newTripBtn = document.querySelector("#new-trip-button");
   const newTripModal = document.querySelector("#new-trip-modal");
+  newTripModal.style.display = "none";
+  const closeModalBtn = document.querySelector("#close-modal");
   const toggleModal = function() {
-    let isOpen = false;
+    console.log(newTripModal.style.display);
+    if (newTripModal.style.display === "none") {
+      newTripModal.style.display = "block";
+    } else {
+      newTripModal.style.display = "none";
+    }
   };
   newTripBtn.addEventListener("click", toggleModal);
+  closeModalBtn.addEventListener("click", toggleModal);
 };
 
 const GoDutchApp = new createGoDutchApp();
