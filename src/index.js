aeacd2d586caee5009c7bffda672db31fbaa9647
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-02-29 18:59:14 +0530

    fixed logic for opening and closing the modal

--- src/index.js
+++ src/index.js
@@ -2,23 +2,32 @@ import "./styles.scss";
 
 const createGoDutchApp = function() {
   const addNewModal = document.querySelectorAll(".modal-window");
-
-  const addFriendBtn = document.querySelector(".add-friend-btn");
-  const closeModalBtn = document.querySelectorAll(".close-modal");
+  const addNewBtns = document.querySelectorAll(".add-new-btn");
+  const addexpenseModal = document.querySelector(".add-expense-modal");
+  const addFriendModal = document.querySelector(".add-friend-modal");
+  const closeModalBtns = document.querySelectorAll(".close-modal");
   let isModalOpen = false;
-  const toggleModal = () => {
+  const toggleModal = e => {
     if (!isModalOpen) {
-      addNewModal.style.display = "block";
+      let isExpenseBtnClicked = e.target.classList.contains("add-expense-btn");
+      isExpenseBtnClicked
+        ? addexpenseModal.classList.add("open-modal")
+        : addFriendModal.classList.add("open-modal");
       isModalOpen = !isModalOpen;
     }
   };
-  const closeModal = () => {
-    addNewModal.style.display = "block";
+  const closeModal = e => {
+    let isCancelExpenseBtnClicked = e.target.classList.contains(
+      "close-expense-modal"
+    );
+    isCancelExpenseBtnClicked
+      ? addexpenseModal.classList.remove("open-modal")
+      : addFriendModal.classList.remove("open-modal");
     isModalOpen = !isModalOpen;
   };
 
-  addFriendBtn.addEventListener("click", toggleModal);
-  closeModalBtn.forEach(btn => btn.addEventListener("click", closeModal));
+  addNewBtns.forEach(btn => btn.addEventListener("click", toggleModal));
+  closeModalBtns.forEach(btn => btn.addEventListener("click", closeModal));
 };
 
 const goDutchApp = new createGoDutchApp();
