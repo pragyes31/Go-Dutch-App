46400a86002754f5edb7937fed31c4db40938243
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-02-29 17:47:22 +0530

    logic for modal windows

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.scss";
+
+const createGoDutchApp = function() {
+  const addNewModal = document.querySelectorAll(".modal-window");
+
+  const addFriendBtn = document.querySelector(".add-friend-btn");
+  const closeModalBtn = document.querySelectorAll(".close-modal");
+  let isModalOpen = false;
+  const toggleModal = () => {
+    if (!isModalOpen) {
+      addNewModal.style.display = "block";
+      isModalOpen = !isModalOpen;
+    }
+  };
+  const closeModal = () => {
+    addNewModal.style.display = "block";
+    isModalOpen = !isModalOpen;
+  };
+
+  addFriendBtn.addEventListener("click", toggleModal);
+  closeModalBtn.forEach(btn => btn.addEventListener("click", closeModal));
+};
+
+const goDutchApp = new createGoDutchApp();
