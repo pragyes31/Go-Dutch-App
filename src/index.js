614de7b0dca3c4895447090192fe1f522fe02b0b
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-03-01 22:53:04 +0530

    Created html structure for each user's detail and balance shet

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ const createGoDutchApp = function() {
   const addexpenseModal = document.querySelector(".add-expense-modal");
   const addFriendModal = document.querySelector(".add-friend-modal");
   const closeModalBtns = document.querySelectorAll(".close-modal");
+  const addFriendForm = document.querySelector(".add-friend-form");
   let isModalOpen = false;
   const toggleModal = e => {
     if (!isModalOpen) {
+      console.log(e.target.classList);
+      // for(key in e.target.classList) {
+      //   if(e.target.classList[key].indexOf("friend")===-1) {
+      //     let isFriendBtnClicked =
+      //   }
+      // }
       let isExpenseBtnClicked = e.target.classList.contains("add-expense-btn");
       isExpenseBtnClicked
         ? addexpenseModal.classList.add("open-modal")
@@ -26,8 +33,17 @@ const createGoDutchApp = function() {
     isModalOpen = !isModalOpen;
   };
 
+  const addNewFriend = () => {
+    console.log("boom");
+    closeModal();
+  };
+
   addNewBtns.forEach(btn => btn.addEventListener("click", toggleModal));
   closeModalBtns.forEach(btn => btn.addEventListener("click", closeModal));
+  addFriendForm.addEventListener("submit", e => {
+    e.preventDefault();
+    addNewFriend();
+  });
 };
 
 const goDutchApp = new createGoDutchApp();
