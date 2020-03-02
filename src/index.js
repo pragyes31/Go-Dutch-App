6b99a570a33fb4bc62ac16d18db05111fac98b25
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-03-02 13:28:26 +0530

    fixed modal window for add frined button

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import "./styles.scss";
 
 const createGoDutchApp = function() {
-  const addNewModal = document.querySelectorAll(".modal-window");
   const addNewBtns = document.querySelectorAll(".add-new-btn");
   const addexpenseModal = document.querySelector(".add-expense-modal");
   const addFriendModal = document.querySelector(".add-friend-modal");
   const closeModalBtns = document.querySelectorAll(".close-modal");
   const addFriendForm = document.querySelector(".add-friend-form");
   let isModalOpen = false;
+  let userCount = 0;
   const toggleModal = e => {
     if (!isModalOpen) {
-      console.log(e.target.classList);
+      console.log(e.target.parentNode.classList);
       // for(key in e.target.classList) {
       //   if(e.target.classList[key].indexOf("friend")===-1) {
       //     let isFriendBtnClicked =
       //   }
       // }
-      let isExpenseBtnClicked = e.target.classList.contains("add-expense-btn");
+      let isExpenseBtnClicked = e.target.classList.contains(
+        "add-expense-btn" || "expense-btn"
+      );
       isExpenseBtnClicked
         ? addexpenseModal.classList.add("open-modal")
         : addFriendModal.classList.add("open-modal");
@@ -33,17 +35,41 @@ const createGoDutchApp = function() {
     isModalOpen = !isModalOpen;
   };
 
-  const addNewFriend = () => {
-    console.log("boom");
-    closeModal();
+  const addNewFriend = e => {
+    closeModal(e);
   };
 
   addNewBtns.forEach(btn => btn.addEventListener("click", toggleModal));
   closeModalBtns.forEach(btn => btn.addEventListener("click", closeModal));
   addFriendForm.addEventListener("submit", e => {
     e.preventDefault();
-    addNewFriend();
+    addNewFriend(e);
   });
 };
 
 const goDutchApp = new createGoDutchApp();
+
+const demoState = [
+  {
+    userName: "Will",
+    userId: "user-01",
+    expenses: [
+      {
+        type: "flights",
+        paidAmount: 500,
+        paidBy: "Will"
+      }
+    ]
+  },
+  {
+    userName: "Eleven",
+    userId: "user-02",
+    expenses: [
+      {
+        type: "Movie tickets",
+        paidAmount: 100,
+        paidBy: "Rahul"
+      }
+    ]
+  }
+];
