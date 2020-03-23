f880c10fb9648cf45152dae0a9c9cdb3d28e84c4
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-03-24 00:21:56 +0530

    added html structure to AddData component

--- src/Components/AddData.js
+++ src/Components/AddData.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function AddData() {
-  return <div />;
+  return (
+    <div class="add-data">
+      <div class="add-friend">
+        <button class="add-friend-btn add-new-btn">Add new Friend</button>
+      </div>
+      <div class="add-expense">
+        <button class="add-expense-btn add-new-btn">Add new Expense</button>
+      </div>
+    </div>
+  );
 }
--- src/Components/UsersData.js
+++ src/Components/UsersData.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function UsersData() {
-  return <div>T</div>;
+  return <div className="users-data" />;
 }
