91766dc242bc66bf0c53b02d3af86cee5d0b6753
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-04-02 21:26:11 +0530

    added modal window components to the mix

---
+++ src/Components/AddNewExpenseModal.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default AddNewExpenseModal() {
+  return (
+    <div class="add-expense-modal modal-window">
+      <form class="add-expense-form">
+        <header class="modal-header add-expense-header">
+          Add new Expense
+            </header>
+        <div class="expense-name-input input-div">
+          <label for="expense-name">Expense Name</label>
+          <input id="expense-name" type="text" required />
+        </div>
+        <div class="expense-amt-input input-div">
+          <label for="expense-amount">Amount</label>
+          <input id="expense-amount" type="number" required />
+        </div>
+        <div class="expense-partner-input input-div">
+          <label for="expense-partner">Select expense partner</label>
+          <select name="expense-partner" id="expense-partner" required>
+            <option value="choose">-Choose a friend-</option>
+          </select>
+        </div>
+        <div class="payer-input input-div">
+          <label for="payer">Paid by:</label>
+          <select name="payer" id="payer" required></select>
+        </div>
+        <button type="submit" class="expense-btn modal-btn">
+          Add Expense</button><br />
+        <button
+          type="button"
+          class="close-modal close-expense-modal modal-btn">
+          Cancel
+            </button>
+      </form>
+    </div>
+  )
+}
--- src/Components/AppDashboard.js
+++ src/Components/AppDashboard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AddData from "./AddData";
 import UsersData from "./UsersData";
+import AddNewFriendModal from "./AddNewFriendModal";
 
 export default function AppDashboard() {
   return (
     <div className="app-dashboard">
       <AddData />
       <UsersData />
+      <AddNewFriendModal />
     </div>
   );
 }
