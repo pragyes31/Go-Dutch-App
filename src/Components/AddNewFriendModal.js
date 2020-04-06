662d0ad3eebc5a5e2f8f8c5dcd695e5db99dcf0b
Rahul Nayak <pragyes31@users.noreply.github.com>
2020-04-06 23:16:52 +0530

    converted AddFriendModal to class based component

--- src/Components/AddNewFriendModal.js
+++ src/Components/AddNewFriendModal.js
@@ -1,29 +1,73 @@
 import React from "react";
 import Modal from "react-modal";
 
-const AddNewFriendModal = () => (
-  <Modal
-    isOpen={true}
-    contentLabel="Add New Friend"
-    ariaHideApp={false}
-    class="add-friend-modal modal-window"
-  >
-    <header class="modal-header add-friend-header">Add new Friend</header>
-    <form class="add-friend-form">
-      <div class="friend-name-input">
-        <label for="friend-name">Name:</label>
-        <input id="friend-name" type="text" required />
-      </div>
-      <button type="submit" class="friend-btn modal-btn">
-        Add Friend
-      </button>
-      <br />
-      <button type="button" class="close-modal close-friend-modal modal-btn">
-        Cancel
-      </button>
-    </form>
-  </Modal>
-);
+class AddNewFriendModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.addNewFriend = this.addNewFriend.bind(this);
+  }
+  addNewFriend(e) {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <Modal
+        isOpen={true}
+        contentLabel="Add New Friend"
+        ariaHideApp={false}
+        class="add-friend-modal modal-window"
+      >
+        <header class="modal-header add-friend-header">Add new Friend</header>
+        <form onClick={this.addNewFriend} class="add-friend-form">
+          <div class="friend-name-input">
+            <label for="friend-name">Name:</label>
+            <input id="friend-name" type="text" required />
+          </div>
+          <button type="submit" class="friend-btn modal-btn">
+            Add Friend
+          </button>
+          <br />
+          <button
+            type="button"
+            class="close-modal close-friend-modal modal-btn"
+          >
+            Cancel
+          </button>
+        </form>
+      </Modal>
+    );
+  }
+}
+
+// const AddNewFriendModal = () => {
+//   const addNewFriend = e => {
+//     e.preventDefault();
+//   };
+//   return (
+//     <Modal
+//       isOpen={true}
+//       contentLabel="Add New Friend"
+//       ariaHideApp={false}
+//       class="add-friend-modal modal-window"
+//     >
+//       <header class="modal-header add-friend-header">Add new Friend</header>
+//       <form onClick={addNewFriend} class="add-friend-form">
+//         <div class="friend-name-input">
+//           <label for="friend-name">Name:</label>
+//           <input id="friend-name" type="text" required />
+//         </div>
+//         <button type="submit" class="friend-btn modal-btn">
+//           Add Friend
+//         </button>
+//         <br />
+//         <button type="button" class="close-modal close-friend-modal modal-btn">
+//           Cancel
+//         </button>
+//       </form>
+//     </Modal>
+//   );
+// };
 
 export default AddNewFriendModal;
 
